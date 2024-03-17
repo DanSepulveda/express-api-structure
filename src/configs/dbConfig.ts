d961b6615bbfb3a0915b5899c13fdaf765319874
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2024-03-17 08:33:09 -0300

    mongoose connection

--- src/configs/dbConfig.ts
+++ src/configs/dbConfig.ts
@@ -1,1 +1,21 @@
-'database'
+import mongoose from 'mongoose';
+import { MONGO_URI } from './constants';
+
+const connectDB = (): void => {
+  mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+      console.log('Database connected');
+    })
+    .catch((error) => {
+      let message = 'Unkown error';
+      if (error instanceof Error) {
+        message = error.message;
+      } else if (typeof error === 'string') {
+        message = error;
+      }
+      console.log('DB connection error: ' + message);
+    });
+};
+
+export default connectDB;
