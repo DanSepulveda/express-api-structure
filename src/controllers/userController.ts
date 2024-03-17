ba801e447f67cb6c76d8a1b28fcca11572892bd8
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2024-03-17 09:46:27 -0300

    user controller initial config

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,1 +1,11 @@
-'user controller'
+import { type Request, type Response } from 'express';
+
+export const createUser = (req: Request, res: Response): void => {
+  console.log(req.body);
+  res.send('Create user');
+};
+
+export const getUsers = (req: Request, res: Response): void => {
+  console.log(req.body);
+  res.send('Get users');
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,8 @@
-'routes'
+import express from 'express';
+import userRouter from './userRoutes';
+
+const router = express.Router();
+
+router.use('/users', userRouter);
+
+export default router;
