e41295887a98031254b85b0c63c1af9f1ef5cb91
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2024-03-17 08:32:24 -0300

    fix dotenv import

--- src/configs/serverConfig.ts
+++ src/configs/serverConfig.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import router from '../routes';
 import connectDB from './dbConfig';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-dotenv.config();
 connectDB();
 
 app.use('/api', router);
