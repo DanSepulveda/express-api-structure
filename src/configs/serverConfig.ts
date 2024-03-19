367baf745eed0a213f6c2761de5d7dae98c9d032
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2024-03-18 22:32:14 -0300

    helmet added

--- src/configs/serverConfig.ts
+++ src/configs/serverConfig.ts
@@ -3,12 +3,15 @@ import cors from 'cors';
 import 'dotenv/config';
 import router from '../routes';
 import connectDB from './dbConfig';
+import helmet from 'helmet';
 
 const app = express();
 app.use(express.json());
+// TODO: configure cors
 app.use(cors());
+app.use(helmet());
 connectDB();
 
-app.use('/api', router);
+app.use('/api/v1', router);
 
 export default app;
