b42ceebd8f299013fa05a6bef053181a16a8c7c2
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2024-03-18 21:05:35 -0300

    signup feature created

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,22 @@
 import { type Request, type Response } from 'express';
+import { userService } from '../services/userService';
+import HttpError from 'http-errors';
 
-export const createUser = (req: Request, res: Response): void => {
-  console.log(req.body);
-  res.send('Create user');
-};
-
-export const getUsers = (req: Request, res: Response): void => {
-  console.log(req.body);
-  res.send('Get users');
+export const createUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { username, password } = req.body;
+    const { success, message, token } = await userService.signup({
+      username,
+      password
+    });
+    res.json({ success, message, token });
+  } catch (error: unknown) {
+    if (error instanceof HttpError.HttpError) {
+      const { message, status } = error;
+      res.status(status).json({ success: false, error: message });
+    }
+  }
 };
---
+++ src/services/userService.ts
@@ -0,0 +1,31 @@
+import User from '../models/User';
+import { generateSalt, hash } from '../utils/registerUser';
+import createHttpError from 'http-errors';
+
+interface UserData {
+  username: string;
+  password: string;
+}
+
+export const userService = {
+  signup: async (
+    user: UserData
+  ): Promise<{ success: boolean; message: string; token: string | null }> => {
+    const { username, password } = user;
+    const isRegistered = await Promise.resolve(User.isRegistered(username));
+    if (isRegistered) throw createHttpError(400, 'Username already exists');
+    const salt = generateSalt();
+    const hashedPassword = hash(salt, password);
+    const newUser = new User({
+      sign: { username, hashedPassword, salt }
+    });
+    await newUser.save();
+    const token = newUser.generateJWT();
+
+    return {
+      success: true,
+      message: 'User created successfully',
+      token
+    };
+  }
+};
