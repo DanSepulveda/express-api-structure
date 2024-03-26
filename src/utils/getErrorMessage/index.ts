9803d1d9d84cf654055b1996079864f0b52502f2
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2024-03-25 22:04:10 -0300

    add: function to get Error message

---
+++ src/utils/getErrorMessage/index.ts
@@ -0,0 +1,11 @@
+export const getErrorMessage = (error: unknown): string => {
+  let message = 'Unkown error';
+
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (typeof error === 'string') {
+    message = error;
+  }
+
+  return message;
+};
