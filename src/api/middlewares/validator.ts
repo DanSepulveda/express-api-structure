17b38ac065818723292e4ed0a3687f082798f729
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2024-03-26 22:38:26 -0300

    project structure changed

---
+++ src/api/middlewares/validator.ts
@@ -0,0 +1,27 @@
+import joi from 'joi';
+import type { Request, Response, NextFunction } from 'express';
+
+const joiSchema = joi.object();
+
+export const validator = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  schema: typeof joiSchema
+): void => {
+  const validation = schema.validate(req.body, { abortEarly: false });
+
+  if (validation.error == null) {
+    next();
+  } else {
+    res.status(400).json({
+      success: false,
+      errors: validation.error.details.map((error) => {
+        return {
+          field: error?.context?.key,
+          message: error.message.replace('"', '{').replace('"', '}')
+        };
+      })
+    });
+  }
+};
