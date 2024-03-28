d048b93fe40cd8bce2922192bf474a85d2a995b1
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2024-03-28 20:49:13 -0300

    cors config added

--- src/api/server.ts
+++ src/api/server.ts
@@ -4,12 +4,24 @@ import cors from 'cors';
 import 'dotenv/config';
 import router from './routes';
 import connectDB from '../config/database';
-import { ENDPOINT_BASE } from '../config/constants';
+import { ACCEPTED_ORIGINS, ENDPOINT_BASE } from '../config/constants';
 
 const app = express();
 app.use(express.json());
-// TODO: configure cors
-app.use(cors());
+
+app.use(
+  cors({
+    origin(requestOrigin, callback) {
+      const origin = requestOrigin ?? 'same origin';
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by cors'));
+      }
+    }
+  })
+);
+
 app.use(helmet());
 connectDB();
 
