506c3a049158c9023c35cb4c5f3f4252271f8edb
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2024-03-29 15:33:09 -0300

    adding modules routes

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import authRouter from './components/auth/routes';
+import permissionRouter from './components/permission/routes';
+import roleRouter from './components/role/routes';
 import userRouter from './components/user/routes';
 
 const router = express.Router();
 
+router.use('/auth', authRouter);
+router.use('/permission', permissionRouter);
+router.use('/role', roleRouter);
 router.use('/users', userRouter);
 
 export default router;
