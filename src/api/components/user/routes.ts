5f9b470fc895017e6c53e8063e6070f1f7b6ebe6
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2024-04-01 06:42:02 -0300

    types added to controller

--- src/api/components/user/routes.ts
+++ src/api/components/user/routes.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, { type RequestHandler } from 'express';
 import { createUser } from './controller';
 import { signupValidator } from './validations';
 
 const userRouter = express.Router();
 
-userRouter.route('/').post(signupValidator, createUser);
+userRouter.route('/').post(signupValidator, createUser as RequestHandler);
 
 export default userRouter;
