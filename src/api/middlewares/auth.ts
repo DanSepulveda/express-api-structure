1cc4cd89329a8a4dc2bff8e3d0bca7366d2aca9a
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2024-04-01 06:37:06 -0300

    auth middleware added

---
+++ src/api/middlewares/auth.ts
@@ -0,0 +1,32 @@
+import passport from 'passport';
+import User from '../components/user/model';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { SECRET_OR_KEY } from '../../config/constants';
+
+const jwtStrategy = new Strategy(
+  {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: SECRET_OR_KEY
+  },
+  (payload, done) => {
+    User.findById(payload.userId)
+      .then((res) => {
+        if (res == null) {
+          done(null, false);
+        } else {
+          done(null, res);
+        }
+      })
+      .catch((error) => {
+        done(error, false);
+      });
+  }
+);
+
+passport.use(jwtStrategy);
+
+const auth = (): void => {
+  passport.authenticate('jwt', { session: false });
+};
+
+export default auth;
