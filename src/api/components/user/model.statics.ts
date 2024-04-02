f7c41fd0d08ced6fddf4949cba3273ea40ec71da
Daniel Sepúlveda <dansepdev@gmail.com>
2024-04-02 00:18:31 -0300

    fix: schema statics structure

--- src/api/components/user/model.statics.ts
+++ src/api/components/user/model.statics.ts
@@ -1,12 +1,13 @@
 import type { userSchema } from './model';
 
 const setStatics = (schema: typeof userSchema): void => {
-  schema.statics.isRegistered = async function (
-    email: string
-  ): Promise<boolean> {
-    const user = await this.findOne({ 'sign.email': email }, '_id');
-    return Boolean(user);
-  };
+  schema.static(
+    'isRegistered',
+    async function isRegistered(email: string): Promise<boolean> {
+      const user = await this.findOne({ 'account.email': email }, '_id');
+      return Boolean(user);
+    }
+  );
 };
 
 export default setStatics;
