4f02f29e62ba8e170c28517857974c6b51922cd3
Daniel Sepúlveda <dansepdev@gmail.com>
2024-04-02 00:17:18 -0300

    comparePassword method added

--- src/api/components/user/model.methods.ts
+++ src/api/components/user/model.methods.ts
@@ -1,19 +1,29 @@
 import jwt from 'jsonwebtoken';
 import type { userSchema } from './model';
 import { SECRET_OR_KEY } from '../../../config/constants';
+import crypto from 'crypto';
 
 const setMethods = (schema: typeof userSchema): void => {
-  schema.methods.generateJWT = function (): string {
+  schema.method('generateJWT', function generateJWT(): string {
     return jwt.sign(
       {
         userId: this._id,
         names: this.basicData.names,
         surename: this.basicData.surename,
-        username: this.sign.username
+        email: this.account.email
       },
       SECRET_OR_KEY
     );
-  };
+  });
+  schema.method(
+    'comparePassword',
+    function comparePassword(password: string): boolean {
+      const hash = crypto
+        .pbkdf2Sync(password, this.account.salt, 10000, 512, 'sha512')
+        .toString('hex');
+      return this.account.hashedPassword === hash;
+    }
+  );
 };
 
 export default setMethods;
