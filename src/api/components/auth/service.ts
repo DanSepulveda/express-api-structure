f87c508e402c0f044701fc7db9c39633abda4c98
Daniel Sepúlveda <dansepdev@gmail.com>
2024-04-02 02:12:36 -0300

    fix: api response messages

--- src/api/components/auth/service.ts
+++ src/api/components/auth/service.ts
@@ -2,11 +2,12 @@ import User from '../user/model';
 import createHttpError from 'http-errors';
 import { generateSalt, hash } from '../../../utils/registerUser';
 import type { BaseResponse, LoginRes, SignupReq } from './types';
+import { USER_MSG } from '../responseMessages';
 
 export const signup = async (user: SignupReq): Promise<BaseResponse> => {
   const { email, password } = user;
   const isRegistered = await Promise.resolve(User.isRegistered(email));
-  if (isRegistered) throw createHttpError(400, 'Email already in use');
+  if (isRegistered) throw createHttpError(400, USER_MSG.registeredAccount);
   const salt = generateSalt();
   const hashedPassword = hash(salt, password);
   const newUser = new User({
@@ -16,22 +17,24 @@ export const signup = async (user: SignupReq): Promise<BaseResponse> => {
 
   return {
     success: true,
-    message: 'User created successfully'
+    message: USER_MSG.signupSuccess
   };
 };
 
 export const login = async (loginData: SignupReq): Promise<LoginRes> => {
   const { email, password } = loginData;
   const user = await User.findOne({ 'account.email': email });
-  if (user == null) throw createHttpError(400, 'User is not registered');
-  if (!user.account.verified) throw createHttpError(400, 'Verification needed');
-  if (!user.account.active) throw createHttpError(400, 'Account disabled');
+  if (user == null) throw createHttpError(400, USER_MSG.noRegisteredAccount);
+  if (!user.account.verified)
+    throw createHttpError(400, USER_MSG.unverifiedAccount);
+  if (!user.account.active)
+    throw createHttpError(400, USER_MSG.disabledAccount);
   const match = user.comparePassword(password);
-  if (!match) throw createHttpError(400, 'Wrong password');
+  if (!match) throw createHttpError(400, USER_MSG.wrongPassword);
 
   return {
     success: true,
-    message: 'User logged in successfully',
+    message: USER_MSG.loginSuccess,
     token: user.generateJWT()
   };
 };
---
+++ src/api/components/responseMessages.ts
@@ -0,0 +1,9 @@
+export const USER_MSG = {
+  registeredAccount: 'Email already in use',
+  signupSuccess: 'User created successfully',
+  noRegisteredAccount: 'User is not registered',
+  unverifiedAccount: 'Verification needed',
+  disabledAccount: 'Account disabled',
+  wrongPassword: 'Wrong password',
+  loginSuccess: 'User logged in successfully'
+};
