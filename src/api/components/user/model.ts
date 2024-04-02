9be8518cb21b3450e189308a6b9181e2cf4867a3
Daniel Sepúlveda <dansepdev@gmail.com>
2024-04-02 00:16:44 -0300

    fix: schema structure

--- src/api/components/user/model.ts
+++ src/api/components/user/model.ts
@@ -1,41 +1,22 @@
-import type { User, UserDoc, UserModel } from './types';
-import { Schema, model, type SchemaDefinition } from 'mongoose';
+import type { UserDoc, UserModel } from './types';
+import { Schema, model } from 'mongoose';
 import setMethods from './model.methods';
 import setStatics from './model.statics';
 
-const schemaFields: SchemaDefinition<User> = {
+export const userSchema = new Schema<UserDoc>({
   account: {
     email: { type: String, required: true, index: true },
     salt: { type: String, required: true },
     hashedPassword: { type: String, required: true },
     verified: { type: Boolean, required: true, default: false },
-    active: { type: Boolean, required: true, default: false }
+    active: { type: Boolean, required: true, default: true }
   },
   basicData: {
     names: String,
     surename: String,
     lastname: String
   }
-};
-
-export const userSchema = new Schema<UserDoc>(schemaFields);
-
-// export const userSchema = new Schema<UserDoc>({
-//   account: {
-//     email: { type: String, required: true, index: true },
-//     salt: { type: String, required: true },
-//     hashedPassword: { type: String, required: true },
-//     verified: { type: Boolean, required: true, default: false },
-//     active: { type: Boolean, required: true, default: false },
-//     lala: String
-//   },
-//   basicData: {
-//     names: String,
-//     surename: String,
-//     lastname: String,
-//     lala: String
-//   }
-// });
+});
 
 setMethods(userSchema);
 setStatics(userSchema);
