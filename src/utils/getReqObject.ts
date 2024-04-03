f1cf01dbcd9f164e535d280f75e97f5c9000945b
Daniel Sepúlveda <dansepdev@gmail.com>
2024-04-03 20:56:20 -0300

    function to get req object to validate

---
+++ src/utils/getReqObject.ts
@@ -0,0 +1,13 @@
+import type { CommonObject, Req } from '../api/types';
+
+export const getReqObject = (req: Req, keys: string[]): CommonObject => {
+  return keys.reduce((obj: CommonObject, key) => {
+    if (
+      Object.keys(req).includes(key) &&
+      (key === 'body' || key === 'params' || key === 'query')
+    ) {
+      obj[key] = req[key];
+    }
+    return obj;
+  }, {});
+};
