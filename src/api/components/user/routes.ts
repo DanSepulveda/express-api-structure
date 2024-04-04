d73b300b2a696d07c05da4a4a4533478200272a0
Daniel Sepúlveda <dansepdev@gmail.com>
2024-04-04 06:05:15 -0300

    router updated

--- src/api/components/user/routes.ts
+++ src/api/components/user/routes.ts
@@ -1,9 +1,5 @@
-import express, { type RequestHandler } from 'express';
-import { createUser } from './controller';
-import { signupValidator } from './validations';
+import express from 'express';
 
 const userRouter = express.Router();
-
-userRouter.route('/').post(signupValidator, createUser as RequestHandler);
 
 export default userRouter;
--- src/api/components/user/validations.ts
+++ src/api/components/user/validations.ts
@@ -1,16 +1,1 @@
-import joi from 'joi';
-import validator from '../../middlewares/validate';
-import type { Request, Response, NextFunction } from 'express';
-
-export const signupValidator = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): void => {
-  const schema = joi.object({
-    username: joi.string().email().trim().required().min(6),
-    password: joi.string().trim().required().min(8)
-  });
-
-  validator(req, res, next, schema);
-};
+'user validations';
