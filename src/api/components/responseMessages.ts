987e600b3d32cae56e9348b900bf7fa5ecb2ae05
Daniel Sepúlveda <dansepdev@gmail.com>
2024-04-15 15:52:14 -0400

    api messages added

--- src/api/components/responseMessages.ts
+++ src/api/components/responseMessages.ts
@@ -1,19 +1,23 @@
-export const USER_MSG = {
-  unverifiedAccount: 'Verification needed',
-  disabledAccount: 'Account disabled',
-  wrongPassword: 'Wrong password',
-  loginSuccess: 'User logged in successfully'
-};
-
 export const AUTH_SUCCESS = {
   signup: 'User created successfully',
-  sendVerifyToken: 'Email sent successfully',
-  verification: 'Account verified successfully'
+  verification: 'Account verified successfully',
+  login: 'User logged in successfully',
+  logout: 'User logged out successfully',
+  sendRecoveryEmail: 'Recovery email sent successfully',
+  sendVerificationEmail: 'Verification email sent successfully',
+  resetPassword: 'Password modified successfully'
 };
 
-export const AUTH_ERROR = {
+export const USER_ERROR = {
   registered: 'Email already in use',
   unregistered: 'User is not registered',
   verified: 'Account already verified',
-  invalidToken: 'Invalid token'
+  unverified: 'Verification needed',
+  disabled: 'Account disabled',
+  wrongPassword: 'Wrong password'
+};
+
+export const TOKEN_ERROR = {
+  invalid: 'Invalid token',
+  expired: 'Expired token'
 };
