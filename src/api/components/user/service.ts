0e71664c6c0f3e42d49d807f0d3d511bf039e955
Daniel Sepúlveda <dansepdev@gmail.com>
2024-04-18 00:07:30 -0400

    function return modified

--- src/api/components/user/service.ts
+++ src/api/components/user/service.ts
@@ -1,21 +1,23 @@
 import createHttpError from 'http-errors';
 import User from './model';
-import type { SignData } from './interfaces';
+import type { SignData, UserDoc } from './interfaces';
 import { USER_ERROR } from '@api/responseMessages';
 
 export const signupWithEmailAndPassword = async (
   data: SignData
-): Promise<void> => {
+): Promise<UserDoc> => {
   const user = await User.findByEmail(data.email, '_id');
   if (user !== null) throw createHttpError(400, USER_ERROR.registered);
   const newUser = new User({ account: { ...data } });
   await newUser.save();
+  return newUser;
 };
 
-export const checkVerifiedStatus = async (email: string): Promise<void> => {
-  const user = await User.findByEmail(email, 'account.verified');
+export const checkVerifiedStatus = async (email: string): Promise<UserDoc> => {
+  const user = await User.findByEmail(email);
   if (user === null) throw createHttpError(404, USER_ERROR.unregistered);
   if (user.account.verified) {
     throw createHttpError(400, USER_ERROR.verified);
   }
+  return user;
 };
