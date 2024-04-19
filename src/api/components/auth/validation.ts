c6e00b91bb83dc2fe58f862e68330b07c2f091ea
Daniel Sepúlveda <dansepdev@gmail.com>
2024-04-19 15:34:41 -0400

    fix: reset password validation change to compare passwords

--- src/api/components/auth/validation.ts
+++ src/api/components/auth/validation.ts
@@ -2,40 +2,24 @@ import joi from 'joi';
 import { email, password } from '@api/commonValidation';
 
 export const signup = {
-  body: joi.object({
-    email,
-    password
-  })
+  body: joi.object({ email, password })
 };
 
 export const login = {
-  body: joi.object({
-    email,
-    password
-  })
-};
-
-export const verifyAccount = {
-  query: joi.object({
-    token: joi.string().required()
-  })
+  body: joi.object({ email, password })
 };
 
 export const forgotPassword = {
-  body: joi.object({
-    email
-  })
+  body: joi.object({ email })
 };
 
 export const resetPassword = {
   body: joi.object({
     password,
-    confirmPassword: password
+    confirmPassword: password.valid(joi.ref('password'))
   })
 };
 
 export const verificationEmail = {
-  body: joi.object({
-    email
-  })
+  body: joi.object({ email })
 };
