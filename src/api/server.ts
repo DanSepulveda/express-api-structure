3e72e99b280602c2e0b3ab49d33f5d1de9e70d98
Daniel Sepúlveda <dansepdev@gmail.com>
2024-04-26 10:29:09 -0400

    compression and cookie-parser dependencies added

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,15 +1,17 @@
 import 'module-alias/register';
 import express from 'express';
-import helmet from 'helmet';
 import cors from 'cors';
+import helmet from 'helmet';
+import passport from 'passport';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import '@config/transporter';
 import router from '@routes/index';
 import connectDB from '@config/database';
-import { ACCEPTED_ORIGINS, ENDPOINT_BASE } from '@config/app';
-import errorHandler from '@middlewares/errorHandler';
-import passport from 'passport';
 import jwtStrategy from '@config/passport';
+import { ACCEPTED_ORIGINS, ENDPOINT_BASE } from '@config/app';
+import errorHandler from '@api/middlewares/httpErrorHandler';
 
 const app = express();
 
@@ -25,9 +27,12 @@ app.use(
       } else {
         callback(new Error('Not allowed by cors'));
       }
-    }
+    },
+    credentials: true
   })
 );
+app.use(cookieParser());
+app.use(compression());
 connectDB();
 app.use(ENDPOINT_BASE, router);
 app.use(errorHandler);
