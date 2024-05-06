a067e9b4878fc270b19ed64c286a9c374b44ce68
Daniel Sepúlveda <dansepdev@gmail.com>
2024-05-06 13:44:08 -0400

    theme integration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,16 @@
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from '@redux/store.ts'
 import { RouterProvider } from 'react-router-dom'
 import router from '@config/router.tsx'
 import './index.css'
+import { ThemeProvider } from '@utils/useThemeContext'
+import theme from '@config/theme'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />,
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </ThemeProvider>,
 )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.emerald,
+        secondary: colors.gray,
+        accent: colors.neutral,
+        info: colors.indigo,
+        warning: colors.amber,
+        error: colors.red,
+      },
+      borderRadius: {
+        normal: '3px',
+      },
+      transitionDuration: {
+        DEFAULT: '200ms',
+      },
+      fontFamily: {
+        heading: ['Ubuntu', 'sans-serif'],
+        text: ['Open Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
