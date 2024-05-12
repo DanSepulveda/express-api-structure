368a5cc776721db83b5ffc1700adfa345f06badb
Daniel Sepúlveda <dansepdev@gmail.com>
2024-05-12 12:08:54 -0400

    styles changed

--- client/src/layouts/LogLayout.tsx
+++ client/src/layouts/LogLayout.tsx
@@ -2,22 +2,21 @@ import Box from '@components/ui-components/Layout/Box'
 import Stack from '@components/ui-components/Layout/Stack'
 import { AFTER_LOGIN_URL } from '@config/app'
 import { appState } from '@redux/app/appSlice'
+import styles from '@styles/global'
 import { useSelector } from 'react-redux'
 import { Navigate, Outlet } from 'react-router-dom'
 
 const LogLayout = () => {
   const { logged } = useSelector(appState)
+  const { sxBox, sxStack } = styles
 
   if (logged) {
     return <Navigate to={AFTER_LOGIN_URL} />
   }
 
   return (
-    <Stack
-      variant="col"
-      tw="min-h-svh bg-pink-200"
-    >
-      <Box tw="w-11/12 sm:w-96">
+    <Stack tw={sxStack.log()}>
+      <Box tw={sxBox.log()}>
         <Outlet />
       </Box>
     </Stack>
