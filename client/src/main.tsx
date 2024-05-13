1b0a5fcbd53007826908df882f7ca33c1fde6c48
Daniel Sepúlveda <dansepdev@gmail.com>
2024-05-13 10:30:14 -0400

    useThemeContext deleted

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,14 +3,10 @@ import { Provider } from 'react-redux'
 import { store } from '@redux/store.ts'
 import { RouterProvider } from 'react-router-dom'
 import router from '@config/router.tsx'
-import './index.css'
-import { ThemeProvider } from '@utils/useThemeContext'
-import theme from '@config/theme'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={theme}>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
 )
