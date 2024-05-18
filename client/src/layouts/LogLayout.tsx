a4b0d917be0a277159d8869b1e3aaf30864d0dca
Daniel Sepúlveda <dansepdev@gmail.com>
2024-05-18 09:59:33 -0400

    styles modified

--- client/src/layouts/LogLayout.tsx
+++ client/src/layouts/LogLayout.tsx
@@ -1,30 +1,22 @@
-// !Third-party libraries
+// * Third-party libraries
 import { useSelector } from 'react-redux'
 import { Navigate, Outlet } from 'react-router-dom'
-// !Styles
-import styles from '@styles/global'
-// !Custom components and hooks
+// * Custom components and hooks
 import Container from '@lib/components/Layout/Container'
 import { appState } from '@redux/app/appSlice'
-// !Config
+// * Config
 import { AFTER_LOGIN_URL } from '@config/app'
 
 const LogLayout = () => {
   const { logged } = useSelector(appState)
-  const { sxContainer } = styles
 
   if (logged) {
     return <Navigate to={AFTER_LOGIN_URL} />
   }
 
   return (
-    <Container
-      tw={
-        sxContainer['col-center-center'] +
-        ' min-h-svh bg-gradient-to-b from-primary-100 to-primary-50'
-      }
-    >
-      <Container tw={sxContainer['box-white'] + ' w-11/12 sm:w-96'}>
+    <Container tw="col-center-center min-h-svh bg-gradient-to-b from-primary-100 to-primary-50">
+      <Container tw="box-white px-4 py-6 w-11/12 sm:w-96">
         <Outlet />
       </Container>
     </Container>
