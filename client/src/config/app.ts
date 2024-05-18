285b8c955e7e576d935e95749164a345e4265216
Daniel Sepúlveda <dansepdev@gmail.com>
2024-05-18 11:10:03 -0400

    app config variables added

--- client/src/config/app.ts
+++ client/src/config/app.ts
@@ -1,3 +1,15 @@
 export const API_BASE_URL = import.meta.env.DEV
   ? 'http://localhost:4000/api/v1'
   : 'Change to deployment url'
+
+// BASIC
+export const MOBILE_NAVBAR_COLOR = '#0a0a0a'
+export const APP_NAME = 'MyApp'
+
+// URL
+export const HOME_URL = '/'
+export const LOGIN_URL = '/login'
+export const AFTER_LOGIN_URL = '/dashboard'
+export const SIGNUP_URL = '/signup'
+export const FORGOT_PWD_URL = '/forgot-password'
+export const NOT_FOUND_URL = '/not-found'
