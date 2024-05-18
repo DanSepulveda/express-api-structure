762bd90cb7194dce85f13a5379da554fdddfdd19
Daniel Sepúlveda <dansepdev@gmail.com>
2024-05-18 11:52:24 -0400

    extend tailwind theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,27 +1,52 @@
 /** @type {import('tailwindcss').Config} */
-import colors from 'tailwindcss/colors'
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: colors.emerald,
-        secondary: colors.gray,
-        accent: colors.neutral,
-        info: colors.indigo,
-        warning: colors.amber,
-        error: colors.red,
-      },
-      borderRadius: {
-        normal: '3px',
+        primary: {
+          50: withOpacity('--color-primary-50'),
+          100: withOpacity('--color-primary-100'),
+          200: withOpacity('--color-primary-200'),
+          300: withOpacity('--color-primary-300'),
+          400: withOpacity('--color-primary-400'),
+          500: withOpacity('--color-primary-500'),
+          600: withOpacity('--color-primary-600'),
+          700: withOpacity('--color-primary-700'),
+          800: withOpacity('--color-primary-800'),
+          900: withOpacity('--color-primary-900'),
+        },
       },
       transitionDuration: {
         DEFAULT: '200ms',
       },
       fontFamily: {
-        heading: ['Ubuntu', 'sans-serif'],
         text: ['Open Sans', 'sans-serif'],
+        heading: ['Ubuntu', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      animation: {
+        enter: 'enter 200ms ease-out',
+        leave: 'leave 150ms ease-in forwards',
+      },
+      keyframes: {
+        enter: {
+          '0%': { transform: 'scale(0.9)', opacity: 0 },
+          '100%': { transform: 'scale(1)', opacity: 1 },
+        },
+        leave: {
+          '0%': { transform: 'scale(1)', opacity: 1 },
+          '100%': { transform: 'scale(0.9)', opacity: 0 },
+        },
       },
     },
   },
