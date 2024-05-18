327ab28639aa098e76e077bb0d3006be48508026
Daniel Sepúlveda <dansepdev@gmail.com>
2024-05-18 11:09:04 -0400

    not equal message added

--- client/src/config/yupErrors.ts
+++ client/src/config/yupErrors.ts
@@ -2,6 +2,7 @@ const yupErrors = {
   email: 'Invalid email format',
   required: 'Required field',
   min8: 'Too short. At least 8 characters',
+  notEqual: 'Both passsword must be equal',
 }
 
 export default yupErrors
