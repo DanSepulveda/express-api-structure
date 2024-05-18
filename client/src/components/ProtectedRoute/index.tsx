1165d83e6753405dcba9389cbd41c4b885b4ea7f
Daniel Sepúlveda <dansepdev@gmail.com>
2024-05-18 12:28:42 -0400

    render unauthorized components instead of redirect

--- client/src/components/ProtectedRoute/index.tsx
+++ client/src/components/ProtectedRoute/index.tsx
@@ -1,5 +1,12 @@
+// * Third-party libraries
 import type { ReactNode } from 'react'
-import { Navigate } from 'react-router-dom'
+import { AiOutlineWarning } from 'react-icons/ai'
+// * Custom components
+import Container from '@lib/components/Layout/Container'
+import Heading from '@lib/components/Typography/Heading'
+import Text from '@lib/components/Typography/Text'
+import Link from '@lib/components/Navigation/Link'
+import { AFTER_LOGIN_URL } from '@config/app'
 
 interface ProtectedRouteProps {
   permission: string | null
@@ -7,13 +14,28 @@ interface ProtectedRouteProps {
 }
 
 const ProtectedRoute = ({ permission, children }: ProtectedRouteProps) => {
-  const permissions: string[] = ['dashboard']
+  // TODO: get permissions from DB when logging in
+  const permissions: string[] = ['dashboard', 'records']
 
   if (permission === null || permissions.includes(permission)) {
     return children
   }
 
-  return <Navigate to="/" />
+  return (
+    <Container tw="h-full col-center-center">
+      <AiOutlineWarning className="text-yellow-500 text-6xl mb-4" />
+      <Heading tw="heading-subtitle mb-4">401 - Unauthorized</Heading>
+      <Text tw="text-large mb-8">
+        You do not have the necessary permissions to access this page.
+      </Text>
+      <Link
+        to={AFTER_LOGIN_URL}
+        tw="link"
+      >
+        Return to Dashboard
+      </Link>
+    </Container>
+  )
 }
 
 export default ProtectedRoute
