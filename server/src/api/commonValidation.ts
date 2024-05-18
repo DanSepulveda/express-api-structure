a3e419556ab6505c4f60825a938f74a728cfc939
Daniel Sepúlveda <dansepdev@gmail.com>
2024-05-18 13:52:54 -0400

    custom messages to validation

--- server/src/api/commonValidation.ts
+++ server/src/api/commonValidation.ts
@@ -1,4 +1,10 @@
 import joi from 'joi';
 
-export const email = joi.string().email().trim().required().min(6);
+export const email = joi
+  .string()
+  .email()
+  .trim()
+  .required()
+  .min(6)
+  .messages({ 'string.email': 'Invalid email format' });
 export const password = joi.string().trim().required().min(8);
